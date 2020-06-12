0bce8df7578c41a9afacbbc5edc07de1e256bef9
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-06-12 15:54:05 -0700

    Creates folder models and the submission Schema
    Creates folder models and the Schema for the submission object. This works OK with the dashboard.ejs render. Already refactored from app.js after some tests, that is why the module is exported with mongoose.

---
+++ models/submission.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var submissionSchema = mongoose.Schema({
+	pp: String,
+	week1: Number,
+	week2: Number,
+	missed: Number,
+	total: Number,
+	description: String,
+	submitted: Boolean,
+	dateSubmitted: String,
+	approved: Boolean,
+	dateApproved: String,
+	ppClosed: Boolean,
+	approvalClosed: Boolean
+});
+
+module.exports = mongoose.model("Submission", submissionSchema);
