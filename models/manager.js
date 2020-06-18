174a8d90ab1f2c34122957aee1033386a6ae6190
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-06-17 21:34:39 -0700

    Updates manager.js
    Adds two more properties to the user schema which will facilitate queries with mongoose and data associations.

--- models/manager.js
+++ models/manager.js
@@ -2,13 +2,15 @@ var mongoose = require("mongoose");
 
 var managerSchema = mongoose.Schema({
 	email: String,
-	name: String
+	name: String,
 	interns: [
 		{
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "Intern"
 		}
-	]
+	],
+	activeStatus: Boolean,
+	payPeriodsAvailable: []
 });
 
 module.exports = mongoose.model("Manager", managerSchema);
