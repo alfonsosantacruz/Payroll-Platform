e0941bc6296dfed758fb47f74dc2a9422caaa3ec
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-06-25 22:26:28 -0700

    Creates apis directory and stats.js
    The apis directory aims to store internal apis for the application. The stats.js file, for example, exports an asynchronous function to recalculate the total hours, and average hours worked per week. It is important to keep track of these stats to comply with the contracts.

---
+++ apis/stats.js
@@ -0,0 +1,38 @@
+var Intern = require("../models/intern"),
+	Submission = require("../models/submission");
+
+module.exports = {
+	calcStats: async function calculateStats(inputEmail){
+		let totalHours = 0
+		let average = 0
+		Intern.findOne({email: inputEmail}, function(err, intern){
+			if (err) {
+				console.log(err);;
+			} else {
+				const internSubmissions = intern.submissions;
+				Submission.find({
+					_id: { $in: internSubmissions}
+				}, function(err, submissions){
+					if(err){
+						console.log(err);
+					} else {
+						submissions.forEach(function(sub, index){
+							// console.log(sub.total);
+							totalHours += sub.total;
+						});
+					} average = totalHours/(submissions.length*2)
+					  // console.log(average);
+					  // console.log(totalHours);
+					  intern.set({totalHours: totalHours, average: average});
+					  intern.save(function(err, updatedIntern){
+					  	if (err) {
+					  		console.log(err);
+					  	} else {
+					  		console.log(updatedIntern);
+					  	}
+					});
+				}); 
+			} 
+		});
+	}
+};
