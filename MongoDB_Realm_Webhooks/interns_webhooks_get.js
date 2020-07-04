d881988616d1b66607738e4f04fdd7e4bfc8f9e5
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-07-04 15:48:52 -0700

    Delete interns_webhooks_get.js
    Accidentally created a duplicate file. Deleting the duplicate.

--- MongoDB_Realm_Webhooks/interns_webhooks_get.js
+++
@@ -1,9 +0,0 @@
-// Set up as GET request method
-// System authentication
-// No additional authentication should be required
-
-exports = function(payload){
-  const mongodb = context.services.get('mongodb-atlas');
-  const collection = mongodb.db("ws_portal").collection("interns");
-  return collection.find({}).toArray();
-}
