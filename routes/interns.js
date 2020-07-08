84579343e86c50811c33ce5a9e8f9237096f0fae
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-07-08 11:04:42 -0600

    Adds remaining routes files
    Includes interns.js and managers.js routes that includes the routes for each user. Some of these routes are shared but not repeated.

---
+++ routes/interns.js
@@ -0,0 +1,143 @@
+var express = require("express"),
+	router = express.Router(),
+	Intern = require("../models/intern"),
+	Submission = require("../models/submission"),
+	Manager = require("../models/manager"),
+	gsheetAPIs = require("../apis/gsheetAPIs"),
+	stats = require("../apis/stats");
+
+
+router.get("/", isLoggedIn, function(req, res){
+	var absoluteEmail = res.locals.email;
+	Intern.findOne({email: absoluteEmail}, function(err, intern){
+		if(err) {
+			console.log(err);
+		} else if (intern == null){
+			console.log('Found null intern');
+			console.log(intern);
+			Manager.findOne({email: absoluteEmail}, function(err, manager){
+				if(err) {
+					console.log(err);
+				} else if (manager == null) {
+					console.log('Found null manager')
+					res.redirect("/notFound");
+				} else {
+					console.log(manager);
+					res.redirect("/home")
+				}
+			});
+		} else {
+			res.redirect("/submissions");
+		}
+	});
+});
+
+router.get("/schedule", isLoggedIn, async (req, res, next) => {
+	var absoluteEmail = res.locals.email;
+	Manager.findOne({email: absoluteEmail}, async function(err, foundManager){
+		if(err) {
+			console.log(err);
+		} else if (foundManager == null) {
+			Intern.findOne({email: absoluteEmail}, async function(err, foundIntern){
+				if (err) {
+					console.log(err);
+				} else {
+					try {
+						const rows = await gsheetAPIs.scheduleSheet();
+						res.render("schedule", {rows:rows, user:'Intern'});
+					} catch(err) {
+						console.log(err);
+						next(err);
+					}
+				}
+			});
+		} else {
+			try {
+				const rows = await gsheetAPIs.scheduleSheet();
+				res.render("schedule", {rows: rows, user: 'Manager'});
+			} catch(err) {
+				console.log(err);
+				next(err);
+			}
+		}
+	});
+});
+
+router.get("/submissions", isLoggedIn, function(req, res){
+	var absoluteEmail = res.locals.email;
+	Intern.findOne({email: absoluteEmail}).populate("submissions").exec(async function(err, intern){
+		if (err) {
+			console.log(err);;
+		} else {
+			await stats.calcStats(absoluteEmail);
+			res.render("dashboard", {user: intern});
+		}
+	});
+});
+
+router.post("/edit", function(req, res){
+	const subId = req.body.subId;
+	console.log(subId);
+	// Submission.update({_id: subId}, {$set: {'submitted': false}}); 
+	Submission.findOne({_id: subId}, function(err, foundSubmission){
+		if(err) {
+			console.log(err);
+		} else {
+			foundSubmission.set({submitted : false});
+			foundSubmission.save(function(err, data){
+				if(err){
+					console.log(err);
+				} else {
+					console.log(data);
+					res.redirect("/submissions");
+				}
+			});
+		}
+	});	
+});
+
+router.post("/report", isLoggedIn, function(req, res){
+	const 	reportedWeek1 = parseFloat(req.body.week1),
+			reportedWeek2 = parseFloat(req.body.week2),
+		 	reportedMissed = parseFloat(req.body.missed),
+		 	reportedTotal = reportedWeek1 + reportedWeek2 + reportedMissed,
+		 	reportedJustification = req.body.justification,
+		 	subId = req.body.subId,
+		 	submissionDate = (new Date()).toString(),
+		 	absoluteEmail = res.locals.email;
+	Submission.findOne({_id: subId}, function(err, foundSubmission){
+		if(err) {
+			console.log(err);
+		} else {
+			foundSubmission.set({
+				week1: reportedWeek1,
+				week2: reportedWeek2,
+				missed: reportedMissed,
+				total: reportedTotal,
+				description: reportedJustification,
+				submitted: true,
+				dateSubmitted: submissionDate
+			});
+			foundSubmission.save(async function(err, data){
+				if(err){
+					console.log(err);
+				} else {
+					console.log(data);
+					await stats.calcStats(absoluteEmail);
+					res.redirect("/submissions");
+				}
+			});
+		}
+	});
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+    	console.log(req.user.email);
+    	res.locals.email = req.user.email;
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
---
+++ routes/managers.js
@@ -0,0 +1,292 @@
+var express = require("express"),
+	router = express.Router(),
+	Intern = require("../models/intern"),
+	Submission = require("../models/submission"),
+	Manager = require("../models/manager"),
+	gsheetAPIs = require("../apis/gsheetAPIs"),
+	stats = require("../apis/stats");
+
+
+router.get("/directory", isLoggedIn, function(req, res) {
+	var absoluteEmail = res.locals.email;
+	Manager.findOne({email: absoluteEmail}, function(err, foundManager){
+		if(err) {
+			console.log(err);
+		} else {
+			const internsManager = foundManager.interns;
+			console.log(internsManager);
+			Intern.find({_id: {$in: internsManager}}, function(err, interns){
+				if(err){
+					console.log(err);
+				} else {
+					res.render("directory", {interns: interns});
+				}
+			});
+		}
+	});
+});
+
+router.get("/home", isLoggedIn, async function(req, res){
+	var absoluteEmail = res.locals.email,
+		currentPP = await gsheetAPIs.ppName();
+	console.log(currentPP);
+	Manager.findOne({email: absoluteEmail}, function(err, manager){
+		if(err) {
+			console.log(err);
+		} else {
+			const internsArray = manager.interns;
+			Intern.find({_id: {$in: internsArray}}, async function(err, interns){
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(interns)
+					Submission.find({pp: currentPP}, function(err, activeSubmissions) {
+						if(err) {
+							console.log(err);
+						} else {
+							res.render("managers", {currentPP:currentPP, manager: manager, interns: interns, submissions: activeSubmissions});
+						}
+					});	
+				}
+			});
+		}
+	});
+});
+
+router.get("/home/:pp", isLoggedIn, function(req, res){
+	var absoluteEmail = res.locals.email;
+	const selectedPP = req.params.pp.replace(/%20/g, " ");
+	console.log(selectedPP);
+	Manager.findOne({email: absoluteEmail}, function(err, manager){
+		if(err) {
+			console.log(err);
+		} else {
+			const internsArray = manager.interns;
+			Intern.find({_id: {$in: internsArray}}, function(err, interns){
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(interns)
+					Submission.find({pp: selectedPP}, function(err, activeSubmissions) {
+						if(err) {
+							console.log(err);
+						} else {
+							res.render("managers", {currentPP:selectedPP, manager: manager, interns: interns, submissions: activeSubmissions});
+						}
+					});	
+				}
+			});
+		}
+	});
+});
+
+
+router.get("/submissions/:id", isLoggedIn, function(req, res){
+	var absoluteEmail = res.locals.email;
+	const internId = req.params.id;
+	console.log(internId);
+	Manager.findOne({email: absoluteEmail}, function(err, manager){
+		if(err) {
+			console.log(err);
+		} else {
+			Intern.findOne({_id: internId}).populate("submissions").exec(async function(err, intern){
+				if (err) {
+					console.log(err);;
+				} else {
+					const internSubmissions = intern.submissions;
+					await stats.calcStats(intern.email);
+					res.render("profile", {manager: manager, submission: internSubmissions, user: intern});
+				}
+			});
+		}
+	});
+});
+
+
+router.post("/editByManager", function(req, res){
+	const subId = req.body.subId;
+	const internId = req.body.internId;
+	console.log(subId);
+	console.log(internId);
+	// Submission.update({_id: subId}, {$set: {'submitted': false}}); 
+	Submission.findOne({_id: subId}, function(err, foundSubmission){
+		if(err) {
+			console.log(err);
+		} else {
+			foundSubmission.set({submitted : false, approved: false});
+			foundSubmission.save(function(err, data){
+				if(err){
+					console.log(err);
+				} else {
+					console.log(data);
+					res.redirect("/submissions/" + internId);
+				}
+			});
+		}
+	});	
+});
+
+
+router.post("/manageredit", function(req, res){
+	const subId = req.body.subId;
+	console.log(subId);
+	Submission.findOne({_id: subId}, function(err, foundSubmission){
+		if(err) {
+			console.log(err);
+		} else {
+			foundSubmission.set({approved : false});
+			foundSubmission.save(function(err, data){
+				if(err){
+					console.log(err);
+				} else {
+					console.log(data);
+					res.redirect("/home");
+				}
+			});
+		}
+	});	
+});
+
+router.post("/reportByManager", function(req, res){
+	const 	reportedWeek1 = parseFloat(req.body.week1),
+			reportedWeek2 = parseFloat(req.body.week2),
+		 	reportedMissed = parseFloat(req.body.missed),
+		 	reportedTotal = reportedWeek1 + reportedWeek2 + reportedMissed,
+		 	reportedJustification = req.body.justification,
+		 	subId = req.body.subId,
+		 	submissionDate = (new Date()).toString(),
+		 	internId = req.body.internId;
+ 	Intern.findOne({_id: internId}, function(err, foundIntern){
+ 		if(err) {
+ 			console.log(err);;
+ 		} else {
+ 			Submission.findOne({_id: subId}, function(err, foundSubmission){
+				if(err) {
+					console.log(err);
+				} else {
+					foundSubmission.set({
+						week1: reportedWeek1,
+						week2: reportedWeek2,
+						missed: reportedMissed,
+						total: reportedTotal,
+						description: reportedJustification,
+						submitted: true,
+						dateSubmitted: submissionDate,
+						approved: true,
+						dateApproved: submissionDate
+					});
+					foundSubmission.save(async function(err, data){
+						if(err){
+							console.log(err);
+						} else {
+							console.log(data);
+							await stats.calcStats(foundIntern.email);
+							res.redirect("/submissions/" + internId);
+						}
+					});
+				}
+			});
+ 		}
+ 	});
+});
+
+
+router.post("/approve", function(req, res){
+	const 	reportedWeek1 = parseFloat(req.body.week1),
+			reportedWeek2 = parseFloat(req.body.week2),
+		 	reportedMissed = parseFloat(req.body.missed),
+		 	reportedTotal = reportedWeek1 + reportedWeek2 + reportedMissed,
+		 	reportedJustification = req.body.justification,
+		 	subId = req.body.subId,
+		 	approvalDate = (new Date()).toString(),
+		 	internId = req.body.internId;
+	console.log('Triggered approve route');
+	Submission.findOne({_id: subId}, function(err, foundSubmission){
+		if(err) {
+			console.log(err);
+		} else {
+			foundSubmission.set({
+				week1: reportedWeek1,
+				week2: reportedWeek2,
+				missed: reportedMissed,
+				total: reportedTotal,
+				description: reportedJustification,
+				submitted: true,
+				dateSubmitted: approvalDate,
+				approved: true,
+				dateApproved: approvalDate
+			});
+			foundSubmission.save(async function(err, data){
+				if(err){
+					console.log(err);
+				} else {
+					console.log(data);
+					Intern.findOne({_id: internId}, async function(err, foundIntern){
+						if(err){
+							console.log(err);
+						} else {
+							await stats.calcStats(foundIntern.email);
+							console.log(foundIntern);
+							res.redirect("/home");
+						}
+					});
+				}
+			});
+		}
+	});
+});
+
+
+router.post("/approveall", isLoggedIn, async function(req, res){
+	var absoluteEmail = res.locals.email;
+	var currentPP = await gsheetAPIs.ppName();
+	console.log(currentPP);
+	Manager.findOne({email: absoluteEmail}, function(err, manager){
+		if(err) {
+			console.log(err);
+		} else {
+			const internsArray = manager.interns;
+			Intern.find({_id: {$in: internsArray}}, function(err, interns){
+				if(err) {
+					console.log(err);
+				} else {
+					console.log(interns)
+					Submission.find({pp: currentPP}, function(err, activeSubmissions) {
+						if(err) {
+							console.log(err);
+						} else {
+							const approvalDate = (new Date()).toString()
+							activeSubmissions.forEach(function(submission, index){
+	    						interns.forEach(function(intern, indexIntern){
+	    							if(intern.submissions.includes(submission._id)){
+	    								submission.set({approved: true, dateApproved: approvalDate});
+	    								submission.save(function(err, updatedSubmission){
+	    									if(err) {
+	    										console.log(err);
+	    									} else {
+	    										console.log(updatedSubmission);
+	    									}
+	    								});
+	    							}
+	    						});
+	    					});
+							res.redirect("/home")
+						}
+					});	
+				}
+			});
+		}
+	});
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+    	console.log(req.user.email);
+    	res.locals.email = req.user.email;
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
