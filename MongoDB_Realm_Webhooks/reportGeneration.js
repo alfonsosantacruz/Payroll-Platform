eb852f5149efa000d481471f73a55cd87688926d
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-07-09 16:44:51 -0700

    Update reportGeneration.js
    Adds http request method comment

--- MongoDB_Realm_Webhooks/reportGeneration.js
+++ MongoDB_Realm_Webhooks/reportGeneration.js
@@ -1,3 +1,7 @@
+// Set up as POST request method
+// System authentication
+// No additional authentication should be required
+
 exports = async function(payload) {
     const mongodb = context.services.get('mongodb-atlas');
     const intern = mongodb.db("ws_portal").collection("interns");
