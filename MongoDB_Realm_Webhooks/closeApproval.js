2aa05db155d244b015f5d5fdaa129c67529716e6
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-07-10 11:59:10 -0700

    Updates closeApproval.js
    Modifies the queries and updateMany() mongodb methods to keep the information about approvals untouched when made by human managers instead of automatically by the time based cloud trigger.

--- MongoDB_Realm_Webhooks/closeApproval.js
+++ MongoDB_Realm_Webhooks/closeApproval.js
@@ -5,15 +5,28 @@
 exports = async function(payload){
   const mongodb = context.services.get('mongodb-atlas');
   const submission = mongodb.db("ws_portal").collection("submissions");
-  const submissionsArray = await submission.find({approvalClosed: false, ppClosed: true, pp:payload.query.pp}, {_id: 1}).toArray();
-  const subs = [];
+  const approvedSubs = await submission.find({approvalClosed: false, ppClosed: true, pp:payload.query.pp, approved: true}, {_id: 1}).toArray();
+  const untouchedSubs = await submission.find({approvalClosed: false, ppClosed: true, pp:payload.query.pp, approved: false}, {_id: 1}).toArray();
+  const approvedSubsArray = [];
+  const untouchedSubsArray = [];
   const submissionDate = (new Date()).toString();
   // return submissionsArray;
-  for (let i = 0; i < submissionsArray.length; i++){
-    subs.push(submissionsArray[i]._id)
+  
+  for (let i = 0; i < approvedSubs.length; i++){
+    approvedSubsArray.push(approvedSubs[i]._id)
   }
+  
+  for (let i = 0; i < untouchedSubs.length; i++){
+    untouchedSubsArray.push(untouchedSubs[i]._id)
+  }
+  
   await submission.updateMany(
-    {_id: {$in: subs}},
+    {_id: {$in: approvedSubsArray}},
+    {$set: {approvalClosed: true}}
+  );
+  
+  await submission.updateMany(
+    {_id: {$in: untouchedSubsArray}},
     {$set: {approvalClosed: true, approved: true, dateApproved: submissionDate}}
   );
 }
