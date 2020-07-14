6aaf979f25fb96c8c4692e0b4481544eaf905a80
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-07-13 22:00:53 -0700

    Updates dashboard.js
    Includes a similar function to add the values of the inputs in each card, but now for rows. Since the tags and bootstrap classes changed, it was necessary to change the JQuery as well.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -8,3 +8,14 @@ $('.form-group').on('input', '.mr-sm-1', function(){
 	});
 	$('#total').html(totalSum);
 });
+
+$('td').on('input', '.mr-sm-1', function(){
+	var totalSum = 0;
+	$("td .mr-sm-1").each(function(){
+		var inputVal = $(this).val();
+		if($.isNumeric(inputVal)){
+			totalSum += parseFloat(inputVal);
+		}
+	});
+	$('#total').html(totalSum);
+});
