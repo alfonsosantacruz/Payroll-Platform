9eb4656194877116e017c89bcd25b25432e2a062
asantacruz <50888490+alfonsosantacruz@users.noreply.github.com>
2020-07-27 13:05:00 -0700

    Updates dashboard.js
    Modifies functionality to add the number of hours on each active card. Includes function to also add number of hours for both rows and cards views. Adds function to support popovers according to Bootstrap and popperJS. Includes functions to keep scroll height even after reloading the page.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -19,3 +19,40 @@ $('td').on('input', '.mr-sm-1', function(){
 	});
 	$('#total').html(totalSum);
 });
+
+
+$(window).scroll(function() {
+  sessionStorage.scrollTop = $(this).scrollTop();
+});
+
+$(document).ready(function() {
+  if (sessionStorage.scrollTop != "undefined") {
+    $(window).scrollTop(sessionStorage.scrollTop);
+  }
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+// $(document).ready(function(){
+//   $("body").scroll(function(){
+//   	var scrollVal = document.getElementsByTagName("body")[0].scrollTop;
+//     $("#sTopVal").html(scrollVal);
+//   });
+// });
+
+// function refreshPage () {
+//     var page_y = document.getElementsByTagName("body")[0].scrollTop;
+//     window.location.href = window.location.href.split('?')[0] + '?page_y=' + page_y;
+// }
+
+// $('#submit').on('click', function() {
+// 	alert('Clicked Submit Button');
+//     location.reload();
+//     if ( window.location.href.indexOf('page_y') != -1 ) {
+//         var match = window.location.href.split('?')[1].split("&")[0].split("=");
+//         document.getElementsByTagName("body")[0].scrollTop = match[1];
+//     }
+// });
+
